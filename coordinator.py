9af0f4a020510a00cc902f8bc6676032fc4dc85a
Dominic Jack <thedomjack@gmail.com>
2018-06-15 12:55:18 +1000

    updated for tf_template changes

--- coordinator.py
+++ coordinator.py
@@ -14,6 +14,15 @@ for d in (params_dir, models_dir):
         os.makedirs(d)
 
 
+def get_eval_metric_ops(predictions, labels):
+    import tensorflow as tf
+    from .ops import get_psnr
+    predictions = tf.clip_by_value(predictions, 0, 1)
+    psnr = get_psnr(predictions, labels, 1.0)
+    psnr = tf.metrics.mean(psnr)
+    return dict(psnr=psnr)
+
+
 def load_params(model_id):
     params_path = os.path.join(params_dir, '%s.json' % model_id)
     if not os.path.isfile(params_path):
@@ -26,15 +35,14 @@ def load_params(model_id):
 def get_coordinator(model_id):
     from .data_source import get_data_source
     from .inference_model import get_inference_model
-    from .eval_model import get_eval_model
     from .train_model import get_train_model
     params = load_params(model_id)
     model_dir = os.path.join(models_dir, model_id)
 
     data_source = get_data_source(**params.get('data_source', {}))
     inference_model = get_inference_model(**params.get('inference_model', {}))
-    eval_model = get_eval_model(**params.get('eval_model', {}))
     train_model = get_train_model(**params.get('train_model', {}))
 
     return Coordinator(
-        data_source, inference_model, eval_model, train_model, model_dir)
+        data_source, inference_model, train_model, model_dir,
+        eval_metric_ops_fn=get_eval_metric_ops)
--- train_model.py
+++ train_model.py
@@ -4,6 +4,33 @@ from __future__ import print_function
 
 from tf_template import TrainModel
 import tensorflow as tf
+import numpy as np
+
+
+def get_loss_fn(use_intermediate_losses, step_loss_fn):
+    if use_intermediate_losses:
+        def inference_loss(inference, labels):
+            steps = inference['steps']
+            T = len(steps)
+            losses = []
+            weights = []
+            for t, step in enumerate(steps):
+                weight = 1 / (T - t)
+                loss = step_loss_fn(step, labels)
+                tf.summary.scalar('loss%d' % t, loss, family='step_loss')
+                weighted_loss = weight * loss
+                losses.append(weighted_loss)
+                weights.append(weight)
+            tf.summary.scalar('opt_loss', loss)
+            loss = tf.add_n(losses) / np.sum(weights)
+            return loss
+        return inference_loss
+    else:
+        def inference_loss(inference, labels):
+            loss = step_loss_fn(inference['opt'], labels)
+            tf.summary.scalar('opt_loss', loss)
+            return loss
+        return inference_loss
 
 
 _optimizers = {
@@ -13,6 +40,7 @@ _optimizers = {
 
 def get_train_model(
         batch_size=64, max_steps=10000,
+        use_intermediate_losses=True,
         optimizer_key='adam', learning_rate=1e-3,
         gradient_clip_value=None):
     if optimizer_key not in _optimizers:
@@ -33,4 +61,7 @@ def get_train_model(
                 for g, v in grads_and_vars]
             return optimizer.apply_gradients(grads_and_vars, global_step)
 
-    return TrainModel(get_train_op, batch_size, max_steps)
+    loss_fn = get_loss_fn(
+        use_intermediate_losses, lambda x, y: tf.nn.l2_loss(x - y))
+
+    return TrainModel.from_fns(loss_fn, get_train_op, batch_size, max_steps)
